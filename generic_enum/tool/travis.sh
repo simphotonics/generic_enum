5cb08428a81575b688420f4f1ba273ac26ffe9a4
simphotonics <git@simphotonics.com>
2020-01-23 19:32:17 +0000

    Initial commit.

---
+++ generic_enum/tool/travis.sh
@@ -0,0 +1,55 @@
+#!/bin/bash --
+# Adapted from https://github.com/google/built_value.dart/blob/master/tool/presubmit
+# BSD-3 Clause License file: https://github.com/google/built_value.dart/blob/master/LICENSE
+
+# Colour definitions:
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+CYAN='\033[1;36m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
+RESET='\033[0m'
+
+# Return immediately if a command returns a non-zero result.
+set -e
+
+echo
+echo -e "${BLUE}=== Resolving dependencies $PWD...${RESET}"
+echo
+pub get
+pub upgrade
+
+echo
+echo -e "${CYAN}=== Building $PWD...${RESET}"
+echo
+# Clear any pre-existing build output so package:build doesn't get confused
+# when we use built_value to build itself.
+rm -rf .dart_tool/build/
+grep -q build_runner pubspec.yaml && \
+    pub run build_runner build \
+        --delete-conflicting-outputs \
+        --fail-on-severe
+
+
+echo
+echo -e "${PURPLE}=== Checking Source Code Formatting${RESET} $PWD..."
+echo
+# Overwrite files with formatted content: -w
+# Dry run: -n
+dartfmt -w $(find bin lib test -name \*.dart 2>/dev/null)
+
+echo
+echo -e "${YELLOW}=== Analyzing $PWD...${RESET}"
+echo
+dartanalyzer \
+    --fatal-warnings \
+    --fatal-infos \
+    --packages="$PWD/.packages" \
+    $(find bin lib test -name \*.dart 2>/dev/null)
+
+echo
+echo -e "${GREEN}=== Testing $PWD...${RESET}"
+echo
+# Only run if directory test exists:
+grep -q test pubspec.yaml && \
+pub run test
---
+++ tool/travis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash --
+
+# Return immediately if a command returns a non-zero result.
+set -e
+
+# Folders to be processed
+folders="generic_enum generic_enum_annotation generic_enum_generator \
+    generic_enum_example"
+
+library_folder=$PWD
+
+for folder in $folders; do
+
+  cd "$library_folder/$folder"
+
+  echo $(pwd)
+
+  # Check if local script exist before executing it.
+  if [ -f tool/travis.sh ]; then
+    ./tool/travis.sh
+  fi
+
+done
