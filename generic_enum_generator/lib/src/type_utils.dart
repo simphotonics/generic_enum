f8263003beb81d1250d2cde8f56dd39a7a9786d9
simphotonics <git@simphotonics.com>
2020-01-24 06:31:49 +0000

    Changed generic_enum_generator to ...builder.

--- generic_enum_generator/lib/src/type_utils.dart
+++
@@ -1,7 +0,0 @@
-import 'package:analyzer/dart/element/type.dart'
-    show DartType, ParameterizedType;
-
-/// Returns a [List] of type arguments (of a parametrized type)
-List<DartType> typeArgumentsOf(DartType type) {
-  return type is ParameterizedType ? type.typeArguments : const [];
-}
