4d75bbb2126ab1953aa211c2dcde2de7d5f27904
simphotonics <git@simphotonics.com>
2020-01-25 19:43:05 +0000

    Initial commit.

---
+++ generic_enum_builder/test/src/num_type.dart
@@ -0,0 +1,15 @@
+import 'package:generic_enum/generic_enum.dart';
+import 'package:generic_enum_annotation/generic_enum_annotation.dart';
+
+/// Sample class used as test input in map_generator_test.dart.
+@GenerateBuiltMap()
+class NumType extends GenericEnum<Type> {
+  const NumType._(Type value) : super(value);
+
+  // Non const constructor should throw [GeneratorException]
+  // in [GenericEnumVisitor]
+  NumType(Type value) : super(value);
+
+  static const INTEGER = NumType._(int);
+  static const DOUBEL = NumType._(double);
+}
