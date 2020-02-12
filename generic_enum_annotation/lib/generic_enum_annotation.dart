a33e62d5d92a33c8a3fbd372b7e258df53bb0dc0
simphotonics <git@simphotonics.com>
2020-02-12 20:44:20 +0000

    Added export.

--- generic_enum_annotation/lib/generic_enum_annotation.dart
+++ generic_enum_annotation/lib/generic_enum_annotation.dart
@@ -1,4 +1,5 @@
 library generic_enum_annotation;
 
-export 'src/generate_value_map.dart';
 export 'src/generate_from_json.dart';
+export 'src/generate_identifier.dart';
+export 'src/generate_value_map.dart';
