f132c2f91b4470876d365d01148dd4095f355fa8
simphotonics <git@simphotonics.com>
2020-02-13 09:55:13 +0000

    Added builder identifierBuilder.

--- generic_enum_builder/lib/generic_enum_builder.dart
+++ generic_enum_builder/lib/generic_enum_builder.dart
@@ -4,6 +4,7 @@
 library generic_enum_builder;
 
 import 'package:build/build.dart';
+import 'package:generic_enum_builder/src/identifier_generator.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:generic_enum_builder/src/json_generator.dart';
 import 'package:generic_enum_builder/src/map_generator.dart';
@@ -18,7 +19,14 @@ Builder mapBuilder(BuilderOptions options) =>
 
 /// Builder calling the method [generate] on a [JsonGenerator]
 /// instance.
-/// [JsonGenerator] processes classes annotated with [@GenerateToFromJson].
-/// It generates the functions [_toJson] and [fromJson].
+/// [JsonGenerator] processes classes annotated with [@GenerateFromJson].
+/// It generates the function [_$<ClassName>FromJson].
 Builder jsonBuilder(BuilderOptions options) =>
     SharedPartBuilder([JsonGenerator()], 'json_builder');
+
+/// Builder calling the method [generate] on a [IdentifierGenerator]
+/// instance.
+/// [IdentifierGenerator] processes classes annotated with [@GenerateIdentifier].
+/// It generates the function [_$<ClassName>Identifier()].
+Builder identifierBuilder(BuilderOptions options) =>
+    SharedPartBuilder([IdentifierGenerator()], 'identifier_builder');
