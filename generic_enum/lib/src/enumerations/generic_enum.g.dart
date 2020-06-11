ef2ecc72d7e0b0d24e37a79cb8da0d9998499c33
simphotonics <git@simphotonics.com>
2020-06-11 15:16:20 +0100

    Moved file to src/enumerations.

---
+++ generic_enum/lib/src/enumerations/generic_enum.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'generic_enum.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GenericEnum<T> _$GenericEnumFromJson<T>(Map<String, dynamic> json) {
+  return GenericEnum<T>(
+    _Converter<T>().fromJson(json['value']),
+  );
+}
+
+Map<String, dynamic> _$GenericEnumToJson<T>(GenericEnum<T> instance) =>
+    <String, dynamic>{
+      'value': _Converter<T>().toJson(instance.value),
+    };
