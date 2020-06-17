93d518d3fa009480e3fa10f07b3c4d4608cad095
simphotonics <git@simphotonics.com>
2020-06-17 13:07:44 +0100

    Added json-serialization tests.

--- generic_enum/test/generic_enum_test.dart
+++ generic_enum/test/generic_enum_test.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:test/test.dart';
+import 'src/complex.dart';
 import 'src/name_part.dart';
 import 'src/plane_type.dart';
 
@@ -21,27 +24,58 @@ void main() {
     });
   });
 
-  group('Serialization:', () {
+  group('toJson():', () {
     test('String value', () {
-      expect(
-          NamePart.FIRST_NAME.toJson(), {'value': NamePart.FIRST_NAME.value});
+      expect(NamePart.FIRST_NAME.toJson(), {'key': 0});
     });
     test('Int value', () {
-      expect(PlaneType.AB747.toJson(), {'value': PlaneType.AB747.value});
+      expect(PlaneType.AB747.toJson(), {'key': 1});
+    });
+    test('Complex value', () {
+      expect(ComplexConstant.i.toJson(), {'key': 0});
     });
   });
 
-  group('DeSerialization:', () {
+  group('fromJson():', () {
     test('String value', () {
       expect(
         NamePart.LAST_NAME,
-        NamePart.fromJson({'value': NamePart.LAST_NAME.value}),
+        NamePart.fromJson({'key': 1}),
       );
     });
     test('Int value', () {
       expect(
         PlaneType.AB707,
-        PlaneType.fromJson({'value': PlaneType.AB707.value}),
+        PlaneType.fromJson({'key': 0}),
+      );
+    });
+    test('Complex value', () {
+      expect(ComplexConstant.i, ComplexConstant.fromJson({'key': 0}));
+    });
+  });
+  group('Serialization:', () {
+    test('GenericEnum<Int>', () {
+      expect(jsonEncode(PlaneType.AB707), '{"key":0}');
+    });
+    test('GenericEnum<Complex>', () {
+      expect(jsonEncode(ComplexConstant.i), '{"key":0}');
+    });
+  });
+  group('De-Serialization:', () {
+    test('GenericEnum<Int>', () {
+      expect(
+        PlaneType.AB707,
+        PlaneType.fromJson(
+          jsonDecode('{"key":0}'),
+        ),
+      );
+    });
+    test('GenericEnum<Complex>', () {
+        expect(
+        ComplexConstant.i,
+        ComplexConstant.fromJson(
+          jsonDecode('{"key":0}'),
+        ),
       );
     });
   });
