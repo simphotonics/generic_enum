6d90e2eae150f6415360ba300c39c0a2fff6f4c2
simphotonics <git@simphotonics.com>
2020-07-17 23:14:38 +0100

    Removed type parameter T.

--- generic_enum_builder/lib/src/collectors/collector.dart
+++ generic_enum_builder/lib/src/collectors/collector.dart
@@ -1,16 +1,11 @@
 import 'dart:collection';
 import 'package:analyzer/dart/element/element.dart' show Element;
 import 'package:analyzer/dart/element/visitor.dart' show SimpleElementVisitor;
-import 'package:source_gen/source_gen.dart' show TypeChecker;
 
 /// Generic class that extends [SimpleElementVisitor] and provides
-/// methods for storing and clearing found instances of [E] with static type
-/// matching [T].
-abstract class Collector<T, E extends Element> extends SimpleElementVisitor {
+/// methods for storing and clearing found instances of [E].
+abstract class Collector<E extends Element> extends SimpleElementVisitor {
   Collector();
-
-  /// TypeChecker of type [T].
-  final checker = TypeChecker.fromRuntime(T);
 
   /// List of collected items of type [E].
   final List<E> _collectedItems = [];
