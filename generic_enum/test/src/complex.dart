8305b672b573cba855cbcc41c37f5260330f0d24
simphotonics <git@simphotonics.com>
2020-07-17 23:11:50 +0100

    Fresh build.

--- generic_enum/test/src/complex.dart
+++ generic_enum/test/src/complex.dart
@@ -1,6 +1,8 @@
 import 'package:generic_enum/generic_enum.dart';
+import 'package:exception_templates/exception_templates.dart';
 
-/// Complex number. Tests if json-serialization works for non-native types.
+part 'complex.g.dart';
+
 class Complex {
   const Complex(this.real, this.imag);
   final num real;
@@ -11,69 +13,29 @@ class Complex {
     return '$real + ${imag}i';
   }
 
-  Map<String, dynamic> toJson() {
-    return {'real': real, 'imag': imag};
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Complex &&
+          runtimeType == other.runtimeType &&
+          this.real == other.real &&
+          this.imag == other.imag;
+
+  @override
+  int get hashCode => real.hashCode ^ imag.hashCode;
+}
+
+@GenerateJsonExtension()
+enum ComplexConstant {
+  zero,
+  i,
+}
+
+extension ComplexConstantValue on ComplexConstant {
+  Complex get value {
+    return const {
+      ComplexConstant.zero: Complex(0, 0),
+      ComplexConstant.i: Complex(0, 1),
+    }[this];
   }
-
-  factory Complex.fromJson(Map<String, dynamic> json) =>
-      Complex(json['real'], json['imag']);
 }
-
-@GenerateFromToJson()
-class ComplexConstant extends GenericEnum<Complex> {
-  const ComplexConstant._(Complex value) : super(value);
-
-  static const i = ComplexConstant._(Complex(0, 1));
-
-  /// 5. Provide access to the map _$DirectionValueMap.
-  static Map<Complex, ComplexConstant> get valueMap =>
-      _$ComplexConstantValueMap;
-
-  /// 6. Define the factory constructor `.fromJson`.
-  factory ComplexConstant.fromJson(Map<String, dynamic> json) =>
-      _$ComplexConstantFromJson(json);
-
-  /// 7. Define the method [toJson].
-  Map<String, dynamic> toJson() => _$ComplexConstantToJson(this);
-}
-
-// **************************************************************************
-// JsonGenerator
-// **************************************************************************
-
-/// Converts an instance of [ComplexConstant] to a map [Map<String, dynamic>].
-/// Add the following method to your class definition:
-/// ```
-///  Map<String, dynamic> toJson() => _$ComplexConstantToJson(this);
-/// ```
-Map<String, dynamic> _$ComplexConstantToJson(ComplexConstant instance) =>
-    {'key': _$ComplexConstantValueMap.values.toList().indexOf(instance)};
-
-/// Converts a map [Map<String, dynamic>] to an instance of [ComplexConstant].
-/// Add the following factory constructor to your class definition:
-/// ```
-/// factory ComplexConstant.fromJson(Map<String, dynamic> json) =>
-///   _$ComplexConstantFromJson(json);
-/// ```
-ComplexConstant _$ComplexConstantFromJson(Map<String, dynamic> json) {
-  final key = (json['key']) as int;
-  ComplexConstant instance = _$ComplexConstantValueMap.values.toList()[key];
-  if (instance == null) {
-    throw GenericEnumException(
-        '.fromJson constructor: Could not find matching instance of type ComplexConstant.');
-  }
-  return instance;
-}
-
-// **************************************************************************
-// MapGenerator
-// **************************************************************************
-
-/// Maps a value of type [Complex] to an instance of [ComplexConstant].
-/// Add the following getter to your class definition:
-/// ```
-/// static Map<Complex,ComplexConstant> get valueMap => _$ComplexConstantValueMap;
-/// ```
-final _$ComplexConstantValueMap = Map<Complex, ComplexConstant>.unmodifiable({
-  ComplexConstant.i.value: ComplexConstant.i,
-});
