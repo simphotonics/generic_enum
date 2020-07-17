37d7febc446173efdb5ebd17aa2aa53f3c2ef34c
simphotonics <git@simphotonics.com>
2020-07-17 23:15:21 +0100

    Add check to pass only enums to JsonWriter.

--- generic_enum_builder/lib/src/generators/json_generator.dart
+++ generic_enum_builder/lib/src/generators/json_generator.dart
@@ -1,21 +1,24 @@
 import 'dart:async';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
-import 'package:generic_enum/generic_enum.dart' show GenerateFromToJson;
+import 'package:generic_enum/generic_enum.dart';
 import 'package:source_gen/source_gen.dart';
 
-import 'package:generic_enum_builder/src/writers/json_writer.dart';
+import '../writers/json_writer.dart';
 
-/// Source code generator that builds the function [_$<ClassName>FromJson].
-/// Extends [GeneratorForAnnotation<GenerateFromToJson>] and as such
-/// processes only classes annotated with [@GenerateFromToJson].
-class JsonGenerator extends GeneratorForAnnotation<GenerateFromToJson> {
+/// Source code generator that builds the extension `<EnumName>Json`.
+class JsonGenerator extends GeneratorForAnnotation<GenerateJsonExtension> {
   @override
   FutureOr<String> generateForAnnotatedElement(
     Element element,
     ConstantReader annotation,
     BuildStep buildStep,
   ) {
-    return JsonWriter(element).fromToJson;
+    if (element is! ClassElement) return '';
+    if ((element as ClassElement).isEnum) {
+      return JsonWriter(element).jsonExtension;
+    } else {
+      return '';
+    }
   }
 }
