6b546ec54e1d87f5fcf64d028553c2262db06e69
simphotonics <git@simphotonics.com>
2020-07-17 23:11:20 +0100

    Removed export directives.

--- generic_enum/lib/generic_enum.dart
+++ generic_enum/lib/generic_enum.dart
@@ -1,7 +1,3 @@
-/// Provides [GenericEnum] an enumeration class with generic value type.
 library generic_enum;
 
-export 'src/enumerations/generic_enum.dart';
-export 'src/annotations/generate_value_map.dart';
-export 'src/annotations/generate_from_to_json.dart';
-export 'src/annotations/generate_identifier.dart';
+export 'src/annotations/generate_json_extension.dart';
