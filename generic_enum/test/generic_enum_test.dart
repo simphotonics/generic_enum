c37a866c0b19e7fd860c4ad80a4cad3eabcf8e67
simphotonics <git@simphotonics.com>
2020-07-18 09:27:30 +0100

    Format.

--- generic_enum/test/generic_enum_test.dart
+++ generic_enum/test/generic_enum_test.dart
@@ -45,11 +45,9 @@ void main() {
     test('DpiResolution', () {
       expect(DpiResolution.MEDIUM.jsonEncoded, '{"index":1}');
     });
-     test('jsonEncode()', () {
+    test('jsonEncode()', () {
       expect(jsonEncode(DpiResolution.LOW.toJson()), '{"index":0}');
     });
-
-
   });
   group('De-Serialization:', () {
     test('ComplexConstant', () {
