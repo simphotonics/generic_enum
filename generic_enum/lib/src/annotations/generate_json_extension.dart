bdac771c2e4a8658b58c1b508127b73a9559b61e
simphotonics <git@simphotonics.com>
2020-07-24 07:27:41 +0100

    Updated docs.

--- generic_enum/lib/src/annotations/generate_json_extension.dart
+++ generic_enum/lib/src/annotations/generate_json_extension.dart
@@ -16,16 +16,6 @@ class GenerateJsonExtension {
   /// //    and annotate it with
   /// @GenerateJsonExtension().
   /// enum DpiResolution { LOW, MEDIUM, HIGH }
-  ///
-  /// // 3. Define an extension on the enumeration
-  /// extension DpiResolutionValue on DpiResolution {
-  /// // 4. Define a getter mapping each instance of the enum to its base value.
-  ///   double get value => const {
-  ///         DpiResolution.LOW: 90.0,
-  ///         DpiResolution.MEDIUM: 300.0,
-  ///         DpiResolution.HIGH: 600.0,
-  ///       }[this];
-  /// }
   /// ```
   /// The builder will generate the file `dpi_resolution.g.dart` with content:
   ///
@@ -34,9 +24,9 @@ class GenerateJsonExtension {
   ///
   /// part of 'dpi_resolution.dart';
   ///
-  /// // **************************************************************************
+  /// // **********************************************************************
   /// // JsonGenerator
-  /// // **************************************************************************
+  /// // **********************************************************************
   ///
   /// /// Extension providing the functions `fromJson` and `toJson`.
   /// extension ToDpiResolution on DpiResolution {
