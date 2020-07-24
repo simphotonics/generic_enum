a7158e2fcaa4e51afe7b6615181c226a068a6e0f
simphotonics <git@simphotonics.com>
2020-07-24 07:30:51 +0100

    jsonBuilder => extensionBuilder.

--- generic_enum_builder/lib/generic_enum_builder.dart
+++ generic_enum_builder/lib/generic_enum_builder.dart
@@ -2,12 +2,17 @@ library generic_enum_builder;
 
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
+
 import 'src/generators/json_generator.dart';
+import 'src/generators/value_generator.dart';
 
 /// Returns an instance of `SharedPartBuilder`.
 ///
 /// Builds an extension providing the functions:
 /// `toJson`, `toJsonEncoded` and `fromJson`.
-Builder jsonBuilder(BuilderOptions options) {
-  return SharedPartBuilder([JsonGenerator()], 'json_builder');
+Builder extensionBuilder(BuilderOptions options) {
+  return SharedPartBuilder([
+    ValueGenerator(),
+    JsonGenerator(),
+  ], 'extension_builder');
 }
