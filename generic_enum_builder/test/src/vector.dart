df9e8a65cff3a4592802df1b03066a893ef0c209
simphotonics <git@simphotonics.com>
2020-07-24 08:04:48 +0100

    Added value annotation.

--- generic_enum_builder/test/src/vector.dart
+++ generic_enum_builder/test/src/vector.dart
@@ -4,11 +4,8 @@ import 'package:exception_templates/exception_templates.dart';
 part 'vector.g.dart';
 
 @GenerateJsonExtension()
+@GenerateValueExtension(
+  mappedValueType: ValueType<double>(),
+  mappedValues: const <String>{'3.14', '6.98'},
+)
 enum Vector { alpha, beta }
-
-extension VectorValue on Vector {
-  double get value => {
-        Vector.alpha: 3.14,
-        Vector.beta: 6.98,
-      }[this];
-}
