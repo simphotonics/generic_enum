d03e09b5caac86f187cdf05b8e248e0062025b96
simphotonics <git@simphotonics.com>
2021-03-25 21:15:25 +0000

    Migrated.

--- generic_enum_builder/lib/src/writers/value_writer.dart
+++ generic_enum_builder/lib/src/writers/value_writer.dart
@@ -1,6 +1,5 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
-import 'package:exception_templates/exception_templates.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:quote_buffer/quote_buffer.dart';
 
@@ -12,19 +11,17 @@ class ValueWriter {
     ClassElement element,
     ConstantReader annotation,
   )   : enumType = element.thisType,
-        getterName = annotation.peek('getterName')?.stringValue,
-        mappedGetterName = annotation.peek('mappedGetterName')?.stringValue,
-        mappedValueType = annotation
-            .peek('mappedValueType')
-            ?.objectValue
-            ?.type
-            ?.typeArguments
-            ?.first,
-        mappedValues = annotation
-            .peek('mappedValues')
-            ?.setValue
-            ?.map((item) => item?.toStringValue())
-            ?.toList() {
+        valueType = annotation
+            .read('valueType')
+            .typeValue
+            .toString()
+            .replaceAll(
+                '*', ''), // Workaround: Analyzer might end the type with a *.
+        values = annotation
+            .read('values')
+            .setValue
+            .map((item) => item.toStringValue() ?? '')
+            .toList() {
     // Retrieve accessors returning enum instances.
     // The names `index` and `values` are used by Dart.
     for (final accessor in element.accessors) {
@@ -38,55 +35,44 @@ class ValueWriter {
   /// Type induced by enum.
   final InterfaceType enumType;
 
-  /// Name of string value getter.
-  final String getterName;
-
-  /// Name of mapped value getter.
-  final String mappedGetterName;
-
   /// Static type of mapped value.
-  final DartType mappedValueType;
+  final String valueType;
 
   /// Names of getters returning an enum instance.
   final names = <String>[];
 
   /// Encoded mapped values.
-  final mappedValues;
+  final List<String> values;
 
   /// Returns a `String` representation of the extension `<EnumName>Value`.
   String get valueExtension {
     final b = StringBuffer();
     b.writeln('/// Extension on `$enumType` providing value-getters.');
     b.writeln('extension ${enumType}Value on $enumType{');
-    b.writeln(mappedValue);
+    b.writeln(value);
     b.writeln(stringValue);
     b.writeln(valueMap);
     b.writeln('}');
     return b.toString();
   }
 
-  /// Returns a `String` representing the getter `getterName`.
+  /// Returns a `String` representing the getter `stringValue`.
   String get stringValue {
     final b = StringBuffer();
-    if (!isValidIdentifier(getterName)) {
-      b.write('\\ Getter name $getterName is not a valid identfier.');
-      b.write('\\ Skipped writing getter: $getterName.');
-      return b.toString();
-    }
     // Dart Doc.
     b.writeln(
       '/// Returns the String identifier of an instance of `${enumType}`.',
     );
     // Function declaration
     b.writeln(
-      'String get $getterName => const <$enumType, String>{',
+      'String get stringValue => const <$enumType, String>{',
     );
-    for (final name in names ?? <String>[]) {
+    for (final name in names) {
       b.write('$enumType.$name:');
       b.writeQ(name);
       b.write(',');
     }
-    b.writeln('}[this];');
+    b.writeln('}[this]!;');
     // Function body
     return b.toString();
   }
@@ -103,7 +89,7 @@ class ValueWriter {
       'Map<String, $enumType> get valueMap => const <String, $enumType>{',
     );
     // Function body
-    for (final name in names ?? <String>[]) {
+    for (final name in names) {
       b.writeQ(name);
       b.write(':$enumType.$name');
       b.write(',');
@@ -113,57 +99,44 @@ class ValueWriter {
   }
 
   /// Returns a `String` representing the getter `mappedGetterName`.
-  String get mappedValue {
+  String get value {
     final b = StringBuffer();
     final message = validateInput();
     // Validate input:
-    if (message != null) {
-      if (message.isNotEmpty) {
-        b.writeln(message);
-        b.writeln('// Skipped writing getter: $mappedGetterName.');
-      }
+    if (message.isNotEmpty) {
+      b.writeln(message);
+      b.writeln('// Skipped writing getter \'value\'.');
       return b.toString();
     }
     // Dart Docs.
-    b.writeln('/// Returns the mapped $mappedValueType value of ');
+    b.writeln('/// Returns value of type <$valueType> mapped to');
     b.writeln('/// an instance of `${enumType}`.');
     // Function declaration.
     b.writeln(
-      '$mappedValueType get $mappedGetterName => const <$enumType, $mappedValueType>{',
+      '$valueType get value => const <$enumType, $valueType>{',
     );
     // Function body
     for (var i = 0; i < names.length; i++) {
       b.write('$enumType.${names[i]}:');
-      b.write(mappedValues[i]);
+      b.write(values[i]);
       b.write(',');
     }
-    b.writeln('}[this];');
+    b.writeln('}[this]!;');
     return b.toString();
   }
 
   /// Returns a non-empty `String` if the following variables are invalid:
-  /// * [mappedValueType]
-  /// * [mappedValues]
-  /// * [mappedGetterName]
-  ///
-  /// Returns `null` if the variables are valid.
-  /// Returns an empty `String` if `mappedValueType`
-  /// and `mappedValues` are both null.
+  /// * `valueType`
+  /// * `values`
   String validateInput() {
-    if (mappedValueType == null && mappedValues == null) {
-      return '';
+    if (valueType == 'null') {
+      return '// Found valueType == null. Check the type parameter <T> '
+          'of the annotation \'GenerateValueExtension<T>\'. ';
+    } else if (values.length != names.length) {
+      return '// Can\'t map $names to $values. '
+          'Check the parameter \'values\' of the '
+          'annotation \'GenerateValueExtension\'';
     }
-    if (mappedValueType == null) {
-      return '// Found mappedValueType == null. ';
-    }
-    if (mappedValues == null) {
-      return '// Found mappedValues == null. ';
-    } else if (mappedValues.length != names.length) {
-      return '// Can\'t map $names to $mappedValues.';
-    }
-    if (!isValidIdentifier(mappedGetterName)) {
-      return '// Found mappedGetterName == $mappedGetterName.';
-    }
-    return null;
+    return '';
   }
 }
