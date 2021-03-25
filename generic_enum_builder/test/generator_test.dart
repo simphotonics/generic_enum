4fe082348cfda27c2603dfaaf4c9b1e397fd4a64
simphotonics <git@simphotonics.com>
2021-03-25 21:23:15 +0000

    Migrated to null safety.

--- generic_enum_builder/test/generator_test.dart
+++ generic_enum_builder/test/generator_test.dart
@@ -1,8 +1,92 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'dart:convert';
+
+import 'dart:async';
+
+import 'package:build/build.dart';
+import 'package:crypto/src/digest.dart';
 import 'package:dart_style/dart_style.dart';
-import 'package:generic_enum_builder/src/generators/json_generator.dart';
-import 'package:generic_enum_builder/src/generators/value_generator.dart';
+import 'package:glob/glob.dart';
 import 'package:test/test.dart';
 import 'package:source_gen_test/src/init_library_reader.dart';
+
+import 'package:generic_enum_builder/src/generators/json_generator.dart';
+import 'package:generic_enum_builder/src/generators/value_generator.dart';
+
+class MockBuildStep extends BuildStep {
+  @override
+  Future<bool> canRead(AssetId id) {
+    // TODO: implement canRead
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Digest> digest(AssetId id) {
+    // TODO: implement digest
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<T> fetchResource<T>(Resource<T> resource) {
+    // TODO: implement fetchResource
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<AssetId> findAssets(Glob glob) {
+    // TODO: implement findAssets
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement inputId
+  AssetId get inputId => throw UnimplementedError();
+
+  @override
+  // TODO: implement inputLibrary
+  Future<LibraryElement> get inputLibrary => throw UnimplementedError();
+
+  @override
+  Future<List<int>> readAsBytes(AssetId id) {
+    // TODO: implement readAsBytes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> readAsString(AssetId id, {Encoding encoding = utf8}) {
+    // TODO: implement readAsString
+    throw UnimplementedError();
+  }
+
+  @override
+  void reportUnusedAssets(Iterable<AssetId> ids) {
+    // TODO: implement reportUnusedAssets
+  }
+
+  @override
+  // TODO: implement resolver
+  Resolver get resolver => throw UnimplementedError();
+
+  @override
+  T trackStage<T>(String label, T Function() action,
+      {bool isExternal = false}) {
+    // TODO: implement trackStage
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> writeAsBytes(AssetId id, FutureOr<List<int>> bytes) {
+    // TODO: implement writeAsBytes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> writeAsString(AssetId id, FutureOr<String> contents,
+      {Encoding encoding = utf8}) {
+    // TODO: implement writeAsString
+    throw UnimplementedError();
+  }
+}
 
 /// Testing MapGenerator and JsonGenerator.
 Future<void> main() async {
@@ -16,11 +100,12 @@ Future<void> main() async {
   final jsonGenerator = JsonGenerator();
   final valueGenerator = ValueGenerator();
 
-  String generatedJsonVector = await jsonGenerator.generate(readerVector, null);
+  String generatedJsonVector =
+      await jsonGenerator.generate(readerVector, MockBuildStep());
   generatedJsonVector = DartFormatter().format(generatedJsonVector);
 
   String generatedValueVector =
-      await valueGenerator.generate(readerVector, null);
+      await valueGenerator.generate(readerVector, MockBuildStep());
   generatedValueVector = DartFormatter().format(generatedValueVector);
 
   String expectedJsonVector =
@@ -29,7 +114,7 @@ Future<void> main() async {
       'extension ToVector on Vector {\n'
       '  /// Converts [json] to an instance of `Vector`.\n'
       '  static Vector fromJson(Map<String, dynamic> json) {\n'
-      '    final index = (json[\'index\']) as int;\n'
+      '    final index = (json[\'index\']) as int?;\n'
       '    if (index == null) {\n'
       '      throw ErrorOf<Vector>(\n'
       '          message: \'Error deserializing json to Vector.\',\n'
@@ -51,24 +136,23 @@ Future<void> main() async {
       '\n'
       '  /// Converts `this` to a json encoded `String`.\n'
       '  String get jsonEncoded => \'{"index":\${Vector.values.indexOf(this)}}\';\n'
-      '}\n'
-      '';
+      '}\n';
 
   String expectedValueVector =
       '/// Extension on `Vector` providing value-getters.\n'
       'extension VectorValue on Vector {\n'
-      '  /// Returns the mapped double value of\n'
+      '  /// Returns value of type <double> mapped to\n'
       '  /// an instance of `Vector`.\n'
       '  double get value => const <Vector, double>{\n'
       '        Vector.alpha: 3.14,\n'
       '        Vector.beta: 6.98,\n'
-      '      }[this];\n'
+      '      }[this]!;\n'
       '\n'
       '  /// Returns the String identifier of an instance of `Vector`.\n'
       '  String get stringValue => const <Vector, String>{\n'
       '        Vector.alpha: \'alpha\',\n'
       '        Vector.beta: \'beta\',\n'
-      '      }[this];\n'
+      '      }[this]!;\n'
       '\n'
       '  /// Returns a mapping of instance name to enum instance.\n'
       '  Map<String, Vector> get valueMap => const <String, Vector>{\n'
